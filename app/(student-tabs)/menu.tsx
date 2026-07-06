ba3ab64cc0f09ea86241cce4d1a7c39730725a7a
chandunitanp <webbanu@gmail.com>
2026-07-06 14:18:10 +0530

    Update menu.tsx

--- app/(student-tabs)/menu.tsx
+++ app/(student-tabs)/menu.tsx
@@ -1,4 +1,3 @@
-
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import * as Linking from "expo-linking";
 import { useAuth } from "../../context/AuthContext";
@@ -7,8 +6,11 @@ export default function MenuScreen() {
   const { user } = useAuth();
 
   const branchCode = user?.branchCode;
+  const registrationNumber = user?.personalEmail?.trim();
+
   console.log("USER =", user);
   console.log("branchCode =", branchCode);
+  console.log("registrationNumber =", registrationNumber);
 
   return (
     <View style={styles.container}>
@@ -16,68 +18,56 @@ export default function MenuScreen() {
 
       {/* ================= UG ================= */}
       {branchCode === "UG" && (
-        <>
-          <TouchableOpacity
-            style={styles.card}
-            onPress={() =>
-              Linking.openURL(
-                "https://nitandhra.ac.in/main/Announcements/2026/UG/Academic%20Calender%202026-27%20for%20B.Tech%20Ist%20Year%20Students.pdf"
-              )
-            }
-          >
-            <Text style={styles.text}>
-              Academic Calendar - UG 1st Year
-            </Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.card}
-            onPress={() =>
-              Linking.openURL(
-                "https://nitandhra.ac.in/main/Announcements/2026/UG/Academic%20Calender%202026-27%20for%20B.Tech%20IInd%20to%20IVth%20Year%20Students.pdf"
-              )
-            }
-          >
-            <Text style={styles.text}>
-              Academic Calendar - UG 2nd to 4th Year
-            </Text>
-          </TouchableOpacity>
-        </>
+        <TouchableOpacity
+          style={styles.card}
+          onPress={() =>
+            Linking.openURL(
+              registrationNumber === "925001"
+                ? "http://172.180.6.211:8080/files/Academic%20Calender%202026-27%20for%20B.Tech%20Ist%20Year%20Students.pdf"
+                : "http://172.180.6.211:8080/files/Academic%20Calender%202026-27%20for%20B.Tech%20IInd%20to%20IVth%20Year%20Students.pdf"
+            )
+          }
+        >
+          <Text style={styles.text}>
+            {registrationNumber === "925001"
+              ? "Academic Calendar - UG 1st Year"
+              : "Academic Calendar - UG 2nd to 4th Year"}
+          </Text>
+        </TouchableOpacity>
       )}
 
       {/* ================= PG ================= */}
-      {branchCode === "PG" && (
-        <TouchableOpacity
-          style={styles.card}
-          onPress={() =>
-            Linking.openURL(
-              "https://nitandhra.ac.in/main/Announcements/2026/PG/Academic%20Calender%202026-27%20for%20all%20M.Tech%20Students%20(1).pdf"
-            )
-          }
-        >
-          <Text style={styles.text}>
-            Academic Calendar - PG
-          </Text>
-        </TouchableOpacity>
-      )}
+     {/* ================= PG ================= */}
+{branchCode === "PG" && (
+  <TouchableOpacity
+    style={styles.card}
+    onPress={() =>
+      Linking.openURL(
+        "http://172.180.6.211:8080/files/Academic%20Calender%202026-27%20for%20all%20M.Tech%20Students%20(1).pdf"
+      )
+    }
+  >
+    <Text style={styles.text}>
+      Academic Calendar - PG
+    </Text>
+  </TouchableOpacity>
+)}
 
-      {/* ================= PHD ================= */}
-      {branchCode === "PHD" && (
-        <TouchableOpacity
-          style={styles.card}
-          onPress={() =>
-            Linking.openURL(
-              "https://nitandhra.ac.in/main/Announcements/2026/PHD/Academic%20Calender%202026-27%20for%20all%20PhD%20Scholars.pdf"
-            )
-          }
-        >
-          <Text style={styles.text}>
-            Academic Calendar - PhD
-          </Text>
-        </TouchableOpacity>
-      )}
-
-  
+{/* ================= PHD ================= */}
+{branchCode === "PHD" && (
+  <TouchableOpacity
+    style={styles.card}
+    onPress={() =>
+      Linking.openURL(
+        "http://172.180.6.211:8080/files/Academic%20Calender%202026-27%20for%20all%20PhD%20Scholars.pdf"
+      )
+    }
+  >
+    <Text style={styles.text}>
+      Academic Calendar - PhD
+    </Text>
+  </TouchableOpacity>
+)}
     </View>
   );
 }
